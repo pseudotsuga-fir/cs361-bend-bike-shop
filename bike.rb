0c9aa57ab3925f6e5298d4ae5f277cdd0dea0cbd
pseudotsuga-fir <hepworta@oregonstate.edu>
2021-11-13 16:31:12 -0800

    restructured bike.rb classes to follow the SRP

--- bike.rb
+++ bike.rb
@@ -3,21 +3,34 @@
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight, :rented
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false, pannier)
     @id = id
     @color = color
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
+    @pannier = pannier
   end
 
   def rent!
     self.rented = true
+  end
+
+end
+
+# Pannier
+
+class Pannier
+
+  MAX_CARGO_ITEMS = 10
+
+  attr_accessor :cargo_contents
+
+  def initialize
+    @cargo_contents = []
   end
 
   def add_cargo(item)
@@ -32,7 +45,7 @@ class Bike
     MAX_CARGO_ITEMS
   end
 
-  def pannier_remaining_capacity
+  def remaining_capacity
     MAX_CARGO_ITEMS - self.cargo_contents.size
   end
 
--- main.rb
+++ main.rb
@@ -2,12 +2,13 @@
 
 require_relative 'bike'
 
-bike = Bike.new(1, :pink, 99.99)
+pannier = Pannier.new
+bike = Bike.new(1, :pink, 99.99, pannier)
 
-bike.add_cargo(:apples)
-bike.add_cargo(:water)
-bike.add_cargo(:repair_kit)
+pannier.add_cargo(:apples)
+pannier.add_cargo(:water)
+pannier.add_cargo(:repair_kit)
 
-puts "Space for #{bike.pannier_remaining_capacity} items left."
+puts "Space for #{pannier.remaining_capacity} items left."
 
 bike.rent!
